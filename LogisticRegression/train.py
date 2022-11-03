5e5ac9f82a355ba2bd1ad20f3930cea877137bb6
Martin1458 <martinisbored1@gmail.com>
2022-11-03 23:31:09 +0100

    LogisticRegression

---
+++ LogisticRegression/train.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+class LogisticsRegression():
+
+    def __init__(self, lr=0.001, n_iters=1000):
+        self.lr = lr
+        self.n_iters = n_iters
+        self.weights = None
+        self.bias = None
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(self.features)
+        self.bias = 0
+
+        for _ in range(self.n_iters):
+            linear_pred = np.dot(X, self.weights) + self.bias
+            predictions = sigmoid(linear_pred)
+
+    def predict(self, X)
